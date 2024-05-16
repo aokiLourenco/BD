26d9528f372cf348060b39a4b1eb4f312353f2d0
Anderson Aoki Lourenço <aaokilourenco@ua.pt>
2024-05-16 14:50:14 +0100

    commit

--- BD-P/aula1/ex12/ex12/obj/Debug/net8.0-windows/ex12.AssemblyInfo.cs
+++ BD-P/aula1/ex12/ex12/obj/Debug/net8.0-windows/ex12.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dffc9abfddcfba45c758fa687e56695c08a44fc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc86ceb2029db81dbd2f72d844585d5a4f34f527")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
