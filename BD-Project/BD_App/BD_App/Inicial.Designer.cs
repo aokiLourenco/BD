c5ff8894972fb92685c74b4ee5dd8b8d07ab5caf
Anderson Aoki Lourenço <aaokilourenco@ua.pt>
2024-05-23 14:44:51 +0100

    adfasdfasdf

---
+++ BD-Project/BD_App/BD_App/Inicial.Designer.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using System;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BD_App
+{
+    partial class Inicial : Form
+    {
+        private SqlConnection CN;
+        private TextBox textBox1;
+        private ImageList imageList1;
+        private System.ComponentModel.IContainer components;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+
+        public static class ConnectionManager
+        {
+            public static SqlConnection getSGBDConnection()
+            {
+                return new SqlConnection("data source = tcp:mednat.ieeta.pt\\SQLSERVER,12; Initial Catalog = p1g2; uid = p1g2; password = #Escr@v0sD01t; TrustServerCertificate=true");
+            }
+        }
+    }
+}
+
