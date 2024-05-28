c8df606bfceb642ae12c6ad4c734af84ad13988c
yeusef0202 <118863646+yeusef0202@users.noreply.github.com>
2024-05-29 00:59:28 +0100

    Continue with the adds

--- BD-Project/Project_BD/Project_BD/Add_Dungeons.Designer.cs
+++ BD-Project/Project_BD/Project_BD/Add_Dungeons.Designer.cs
@@ -28,12 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Add_Dungeons";
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            textBox_Name = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox_Area = new TextBox();
+            label3 = new Label();
+            textBox_MainBoss = new TextBox();
+            button2 = new Button();
+            label7 = new Label();
+            textBox_Location = new TextBox();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 7;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45.3947372F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 54.6052628F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 144F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 130F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 140F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 46F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 92F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Controls.Add(textBox_Name, 1, 2);
+            tableLayoutPanel1.Controls.Add(label1, 1, 1);
+            tableLayoutPanel1.Controls.Add(button2, 6, 0);
+            tableLayoutPanel1.Controls.Add(label7, 4, 1);
+            tableLayoutPanel1.Controls.Add(textBox_Location, 4, 2);
+            tableLayoutPanel1.Controls.Add(textBox_Area, 1, 4);
+            tableLayoutPanel1.Controls.Add(label2, 1, 3);
+            tableLayoutPanel1.Controls.Add(textBox_MainBoss, 4, 4);
+            tableLayoutPanel1.Controls.Add(label3, 4, 3);
+            tableLayoutPanel1.Location = new Point(8, 8);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 6;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 79.06977F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20.930233F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 32F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 42F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 249F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(15, 15);
+            button1.Margin = new Padding(15, 15, 3, 3);
+            button1.Name = "button1";
+            button1.Padding = new Padding(0, 1, 0, 0);
+            button1.Size = new Size(94, 46);
+            button1.TabIndex = 0;
+            button1.Text = "Cancel";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Name
+            // 
+            textBox_Name.Anchor = AnchorStyles.Top;
+            textBox_Name.Location = new Point(115, 89);
+            textBox_Name.Name = "textBox_Name";
+            textBox_Name.Size = new Size(129, 23);
+            textBox_Name.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top;
+            label1.AutoSize = true;
+            label1.Location = new Point(160, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top;
+            label2.AutoSize = true;
+            label2.Location = new Point(164, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Area";
+            // 
+            // textBox_Area
+            // 
+            textBox_Area.Anchor = AnchorStyles.Top;
+            textBox_Area.Location = new Point(116, 161);
+            textBox_Area.Name = "textBox_Area";
+            textBox_Area.Size = new Size(127, 23);
+            textBox_Area.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top;
+            label3.AutoSize = true;
+            label3.Location = new Point(562, 126);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 15);
+            label3.TabIndex = 7;
+            label3.Text = "MainBoss";
+            // 
+            // textBox_MainBoss
+            // 
+            textBox_MainBoss.Anchor = AnchorStyles.Top;
+            textBox_MainBoss.Location = new Point(530, 161);
+            textBox_MainBoss.Name = "textBox_MainBoss";
+            textBox_MainBoss.Size = new Size(122, 23);
+            textBox_MainBoss.TabIndex = 8;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button2.ForeColor = SystemColors.ControlText;
+            button2.Location = new Point(714, 15);
+            button2.Margin = new Padding(3, 15, 15, 3);
+            button2.Name = "button2";
+            button2.Padding = new Padding(0, 1, 0, 0);
+            button2.Size = new Size(71, 46);
+            button2.TabIndex = 2;
+            button2.Text = "Confirm";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Top;
+            label7.AutoSize = true;
+            label7.Location = new Point(564, 68);
+            label7.Name = "label7";
+            label7.Size = new Size(53, 15);
+            label7.TabIndex = 15;
+            label7.Text = "Location";
+            // 
+            // textBox_Location
+            // 
+            textBox_Location.Anchor = AnchorStyles.Top;
+            textBox_Location.Location = new Point(530, 89);
+            textBox_Location.Name = "textBox_Location";
+            textBox_Location.Size = new Size(122, 23);
+            textBox_Location.TabIndex = 16;
+            // 
+            // Add_Dungeons
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Add_Dungeons";
+            Text = "Add_Dungeons";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private TextBox textBox_Name;
+        private Label label1;
+        private Button button2;
+        private Label label7;
+        private TextBox textBox_Location;
+        private TextBox textBox_Area;
+        private Label label2;
+        private TextBox textBox_MainBoss;
+        private Label label3;
     }
 }
--- BD-Project/Project_BD/Project_BD/Add_Dungeons.cs
+++ BD-Project/Project_BD/Project_BD/Add_Dungeons.cs
@@ -16,5 +16,7 @@ namespace Project_BD
         {
             InitializeComponent();
         }
+
+        
     }
 }
